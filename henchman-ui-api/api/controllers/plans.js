c79d532897893b5d629b60fd0882d7c056a048c6
Jordan Lin <jlin@apigee.com>
2016-02-05 00:36:10 -0800

    finished making api endpoints for executing plans.  The webpage to print a stream is tricky.  I don't know if a user would prefer something like that though

--- henchman-ui-api/api/controllers/plans.js
+++ henchman-ui-api/api/controllers/plans.js
@@ -4,13 +4,17 @@ var util = require('util');
 var config = require('config');
 var fs = require('fs');
 var plansDir = config.get("henchman.plans")
+var invDir = config.get("henchman.inventories")
+var modDir = config.get("henchman.modules")
 
 module.exports = {
   listPlans: listPlans,
   getPlan: getPlan,
   putPlan: putPlan,
   postPlan: postPlan,
-  deletePlan: deletePlan
+  deletePlan: deletePlan,
+  execPlan: execPlan,
+  getExecStdout: getExecStdout
 };
 
 function listPlans(req, res) {
@@ -73,4 +77,59 @@ function deletePlan(req, res) {
   fs.unlinkSync(path)
 
   res.status(200).send({'message': util.format("Successfully removed '%s' in '%s'", fname, plansDir)})
+}
+
+var henchStdout = ""
+var henchStatus = 0
+var henchLock = 0
+function execPlan(req, res) {
+  var fname = req.swagger.params.file.value
+  var planPath = plansDir + fname
+  var inventory = req.swagger.params.inventory.value
+  var invPath = invDir + inventory
+
+  /*
+  var exec = require('child_process').exec
+  var cmd = util.format("./%s exec %s --inventory %s --modules %s", 
+      config.get("henchman.exec"), planPath, invPath, modDir)
+  exec(cmd, function(err, stdout, stderr) {
+      if (err) {
+        return res.status(400).send({'message': JSON.stringify(err)})
+      }
+      console.log(stdout)
+  });
+
+  res.status(200).send({'message': "success"})
+  */
+  if (henchLock) {
+    return res.status(400).send({'message': 'henchman is running'})
+  }
+
+  henchLock = 1
+  henchStatus = 1
+  henchStdout = ""
+  var spawn = require('child_process').spawn;
+  var child = spawn("./"+config.get("henchman.exec"), [
+    'exec', planPath,
+    '--inventory', invPath,
+    '--modules', modDir
+  ]);
+
+  child.on('error', function(err) {
+      console.log('Failed to start child process.');
+  });
+
+  child.stdout.on('data', function(chunk) {
+    henchStdout += chunk.toString()
+  });
+
+  child.on('close', function(code) {
+    henchStatus = 0
+    henchLock = 0
+  });
+  res.status(200).send({'message': 'henchman has started'})
+}
+
+function getExecStdout(req, res) {
+  res.status(200).send({'data': henchStdout, 'status': henchStatus})
 }
